c8e72d529bc6edaab7d039903101964fa4b113b0
yeray142 <yeraycorderocarrasco@gmail.com>
2024-05-18 21:18:06 +0200

    Moving stuff

--- src/cloud/main.py
+++ src/cloud/main.py
@@ -3,7 +3,7 @@ import logging
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-from speaker_diarization import SpeakerDiarization
+from classes.speaker_diarization import SpeakerDiarization
 
 logging.basicConfig(level=logging.INFO)
 app = FastAPI()
